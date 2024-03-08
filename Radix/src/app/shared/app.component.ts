c066e4ab1cacc1fcb9c8e446dc00b2c57fdf09d9
chrysovalantis.psilos <chrysovalantis.psilos@student.kuleuven.be>
2024-03-08 22:45:34 +0100

    starting implementing UI T-19

--- Radix/src/app/shared/app.component.ts
+++ Radix/src/app/shared/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Radix';
+
+  ngOnInit() {
+    initFlowbite();
+  }
 }
